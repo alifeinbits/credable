14234892af51451488e0622ea41d8d9be318e7ca
Felix Mutua <felix.mutua@icloud.com>
2025-03-22 00:13:58 +0300

    chore:Mock Client Registration

--- src/main/java/com/credable/lms/service/ScoringService.java
+++ src/main/java/com/credable/lms/service/ScoringService.java
@@ -1,5 +1,7 @@
 package com.credable.lms.service;
 
+import com.credable.lms.model.ClientRegistration;
+import com.credable.lms.repository.ClientRegistrationRepository;
 import com.credable.lms.request.ClientRegistrationRequest;
 import com.credable.lms.response.RegisterClientResponse;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +9,7 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -14,16 +17,18 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ScoringService {
     private final RestTemplate restTemplate;
     private final ScoringEngineService scoringEngineService;
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @Value("${scoring.engine.register.url}")
     private String registerUrl;
 
-    public ScoringService(RestTemplate restTemplate, ScoringEngineService scoringEngineService) {
+    public ScoringService(RestTemplate restTemplate, ScoringEngineService scoringEngineService, ClientRegistrationRepository clientRegistrationRepository) {
         this.restTemplate = restTemplate;
         this.scoringEngineService = scoringEngineService;
+        this.clientRegistrationRepository = clientRegistrationRepository;
     }
 
-    /*public RegisterClientResponse registerClient(String endpointUrl, String name, String username, String password) {
+    public RegisterClientResponse registerClient(String endpointUrl, String name, String username, String password) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -33,12 +38,27 @@ public class ScoringService {
         ResponseEntity<RegisterClientResponse> response =
                 restTemplate.exchange(registerUrl, HttpMethod.POST, entity, RegisterClientResponse.class);
 
-        return response.getBody();
-    }*/
+//        return response.getBody();
+        RegisterClientResponse registerClientResponse = mockRegisterClient(endpointUrl, name, username, password);
+        //Save
+        ClientRegistration clientRegistration = new ClientRegistration();
+        clientRegistration.setId(registerClientResponse.getId());
+        clientRegistration.setToken(registerClientResponse.getToken());
+        clientRegistration.setUrl(registerClientResponse.getUrl());
+        clientRegistration.setName(registerClientResponse.getName());
+        clientRegistration.setUsername(registerClientResponse.getUsername());
+        clientRegistration.setPassword(registerClientResponse.getPassword());
+        clientRegistration.setCreatedAt(LocalDateTime.now());
+        clientRegistration.setUpdatedAt(LocalDateTime.now());
+        clientRegistrationRepository.save(clientRegistration);
+
+        return registerClientResponse;
+
+    }
 
     //Mock this response
 
-    public RegisterClientResponse registerClient(String endpointUrl, String name, String username, String password) {
+    public RegisterClientResponse mockRegisterClient(String endpointUrl, String name, String username, String password) {
         return new RegisterClientResponse(
                 ThreadLocalRandom.current().nextLong(100000, 999999),
                 endpointUrl,
